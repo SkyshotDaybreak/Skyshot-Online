1488efe50bf3529b4ecd5b1ea550d460bc67fc59
Sky Majerus <52509484+SkyshotDaybreak@users.noreply.github.com>
2024-10-05 12:05:01 -0500

    renamed open class and added topbar for contact window

--- skysite/script.js
+++ skysite/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $(".border").draggable();
 
-    $(".clickable").click(function(){
-      $("#music").css("display", "block");
+    $(".open").click(function(){
+      $("#contact").css("display", "block");
     });
     $(".close").click(function(){
         $("#contact").css("display", "none");
