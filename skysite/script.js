f6e8a267ca2d7ccb83bc725abbc6b707291fd69f
Sky Majerus <52509484+SkyshotDaybreak@users.noreply.github.com>
2024-10-05 12:40:11 -0500

    draggable only over the top bar script

--- skysite/script.js
+++ skysite/script.js
@@ -1,6 +1,14 @@
-$(document).ready(function () {
-    $(".border").draggable();
+$(document).ready(function(){
+    
+    //draggable only over the topbar
+    $( ".topbar" ).hover( 
+        function() {
+            $(".border").draggable({ disabled: false });},
+        function () { //function 2
+            $(".border").draggable({ disabled: true });}
+      );
 
+    //open/closing window
     $(".open").click(function(){
       $("#contact").css("display", "block");
     });
