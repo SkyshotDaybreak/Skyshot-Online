493ad8cc98316950ef58f85ac55dc1034a228602
Sky Majerus <52509484+SkyshotDaybreak@users.noreply.github.com>
2024-10-15 17:48:40 -0500

    removed ich image

--- skysite/script.js
+++ skysite/script.js
@@ -3,10 +3,11 @@ $(document).ready(function(){
     //draggable only over the topbar
     $( ".topbar" ).hover( 
         function() {
-            $(".border").draggable({ disabled: false });},
-        function () { //function 2
-            $(".border").draggable({ disabled: true });}
-      );
+            $(".border").draggable({ disabled: false });
+        }, function () {
+            $(".border").draggable({ disabled: true });
+        }
+    );
 
     //open/closing window
     $(".open").click(function(){
