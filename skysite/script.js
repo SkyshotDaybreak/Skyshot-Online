2368fed45073f319c1a3639a101736924fe2c8fb
Sky Majerus <52509484+SkyshotDaybreak@users.noreply.github.com>
2026-08-10 14:37:26 -0500

    overhaul. background, format of main content, deprecated "window" features

--- skysite/script.js
+++ skysite/script.js
@@ -1,27 +1,31 @@
 $(document).ready(function(){
     
     //draggable only over the topbar
-    $( ".topbar" ).hover( 
-        function() {
-            $(".border").draggable({ disabled: false });
-        }, function () {
-            $(".border").draggable({ disabled: true });
-        }
-    );
+    // $( ".topbar" ).hover( 
+    //     function() {
+    //         $(".border").draggable({ disabled: false });
+    //     }, function () {
+    //         $(".border").draggable({ disabled: true });
+    //     }
+    // );
 
     //open/closing window
     $("#openContact").click(function(){
-      $("#contact").css("display", "block");
+        $("#contact").css("display", "block");
+        $("main").css("display", "none");
     });
     $("#closeContact").click(function(){
         $("#contact").css("display", "none");
+        $("main").css("display", "block");
     });
     
     $("#openMedia").click(function(){
         $("#media").css("display", "block");
+        $("main").css("display", "none");
       });
     $("#closeMedia").click(function(){
         $("#media").css("display", "none");
+        $("main").css("display", "block");
     });
 });
   
