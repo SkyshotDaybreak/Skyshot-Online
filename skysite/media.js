31d2a14afdf1c8a29e2f5eefe25ba007f8f59ea8
Sky Majerus <52509484+SkyshotDaybreak@users.noreply.github.com>
2026-08-17 16:11:46 -0500

    main body content scroll

--- skysite/media.js
+++ skysite/media.js
@@ -3,65 +3,65 @@ let game_list = [
 {
     title: "Final Fantasy XIV: Heavensward",
     thumb: 2,
-    desc: "",
+    desc: "My second year in Eorzea, I'm about on pace for one expansion per year. As many have acclaimed, Heavensward truely blows A Realm Reborn out of the water. The amped up production in cutscene work and area design were highlights for me.",
 
 },
 {
     title : "Resident Evil 2 Remake",
     thumb : 4,
-    desc : "",
+    desc : "After getting into the survival horror genre a few years ago with Signalis, I was hooked. My first full Resident Evil experience after briefly playing the first REmake on the Gamecube. This game totally sold me on why these games, and especially this remake, is so adored. It managed to often, truely, spook me. ",
 
 },
 {
     title : "Resident Evil 3 Remake",
     thumb : 1,
-    desc : "",
+    desc : "Not as beloved as RE2R. However I still managed to enjoy the brief fun this game delivers. ",
 
 },
 {
     title :"Resident Evil 4",
     thumb : 1,
-    desc : "",
+    desc : "ongoing",
 
 },
 {
     title : "The Legend of Zelda: Tears of the Kingdom",
     thumb : 1,
-    desc : "",
+    desc : "Another game that, as time passed, has come under less favorable criticism. I think to properly enjoy Tears, you probably need a great amount of time since playing Breath of the Wild. When Tears came out, I dropped it about 40 hours in (nothing to scoff at), but I couldn't finish. It took another 2 years after release for me to get really hooked. Even more time since I played BOTW. With this new lens, it seems clear to me that Tears is almost a direct upgrade of a game. I'm obsessed.",
 
 },
 {
     title :"Fallout 3",
     thumb : 1,
-    desc : "",
+    desc : "TOTW. nostalgia unmatched. mid comfort game",
 
 }, {
     title : "Metal Gear Solid V: The Phantom Pain",
     thumb : 1,
-    desc : "",
+    desc : "After playing both MGS2, MGS3, and MGSR, I didn't enjoy MGS5 as much. There are much more interlinked complex game systems in a handful of open zone areas, but I personally prefer the more linear focused experiences of the earlier games. MGS5 is not a bad game by any means, just less hooking for me",
 
 },
 {
     title : "Marathon",
     thumb : 1,
-    desc : "",
+    desc : "beautiful cyber babes",
 
 },
 {
     title : "Halo 4",
     thumb : 1,
-    desc : "",
+    desc : "first palythru. Played this on the Master Chief Collection on Heroic with one of my close friends. I was terribly sick during the experience. decent",
 
 },
 {
     title : "Shin Megami Tensei Devil Summoner Soul Hackers",
     thumb : 1,
-    desc : ""
+    desc : "addicting old school jrpg"
 },
 {
     title : "Miscellaneous",
     thumb : 1,
-    desc : ""
+    desc : "idk brah"
 }
 
 ];
@@ -77,5 +77,5 @@ let musicList = [
 ];
 
 function display(item) { // expected array object
-    $("#mediaList").append("<p>" + item.title + " </p>")
+    $("#mediaList").append("<p>" + "<i>" + item.title + "</i> <br>" + item.desc + " </p><br>")
 }
