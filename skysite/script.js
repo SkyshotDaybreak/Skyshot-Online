0a89bce43e5d9efac66475b67403a05275c2c12b
Skylar Majerus <cruder20@gmail.com>
2026-08-22 14:39:28 -0500

    media tab working

--- skysite/script.js
+++ skysite/script.js
@@ -12,17 +12,30 @@ $(document).ready(function () {
         $("#mainBody").html($("#socials").html());
     });
     $("#openMedia").click(function(){
+        $("#mediaTab").css("display", "block");
+    });
+    $("#mediaGame").click(function(){
         $("#mainBody").html($("#media").html());
         for (let i = 0; i < game_list.length; i++){
             display(game_list[i]);
         }
+        $("#mediaTab").css("display", "none");
     });
-    // $("medGame/mus/book").click(function(){
-    //     $("#mainBody").html($("#media").html());
-    //     for (let i = 0; i < game_list.length; i++){
-    //         display(game_list[i]);
-    //     }
-    // });
+    $("#mediaMusic").click(function(){
+        $("#mainBody").html($("#media").html());
+        for (let i = 0; i < musicList.length; i++){
+            display(musicList[i]);
+        }
+        $("#mediaTab").css("display", "none");
+    });
+    $("#mediaBook").click(function(){
+        $("#mainBody").html($("#media").html());
+        for (let i = 0; i < bookList.length; i++){
+            display(bookList[i]);
+        }
+        $("#mediaTab").css("display", "none");
+
+    });
     
 
 
