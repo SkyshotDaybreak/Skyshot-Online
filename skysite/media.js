0bc43759f0a841d188671fb718cfc6cd4d065862
Sky Majerus <52509484+SkyshotDaybreak@users.noreply.github.com>
2026-08-24 23:06:17 -0500

    updated css for right sidebar

--- skysite/media.js
+++ skysite/media.js
@@ -5,69 +5,69 @@ let game_list = [
     thumb: 2,
     desc: "My second year in Eorzea, I'm about on pace for one expansion per year. As many have acclaimed, Heavensward truely blows A Realm Reborn out of the water. The amped up production in cutscene work and area design were highlights for me.",
     artist: "squeenix",
-    pfm: "PC"
+    pfm: "(PC)"
 },
 {
     title : "Resident Evil 2 Remake",
     thumb : 4,
     desc : "After getting into the survival horror genre a few years ago with Signalis, I was hooked. My first full Resident Evil experience after briefly playing the first REmake on the Gamecube. This game totally sold me on why these games, and especially this remake, is so adored. It managed to often truely spook me. ",
     artist: "",
-    pfm: "PC"
+    pfm: "(PC)"
 },
 {
     title : "Resident Evil 3 Remake",
     thumb : 1,
     desc : "meow",
     artist: "",
-    pfm: "PC"
+    pfm: "(PC)"
 },
 {
     title :"Resident Evil 4",
     thumb : 1,
     desc : "ongoing",
     artist: "",
-    pfm: "PS2"
+    pfm: "(PS2)"
 },
 {
     title : "The Legend of Zelda: Tears of the Kingdom",
     thumb : 1,
     desc : "Another game that, as time passed, has come under less favorable criticism. I think to properly enjoy Tears, you probably need a great amount of time since playing Breath of the Wild. When Tears came out, I dropped it about 40 hours in (nothing to scoff at), but I couldn't finish. It took another 2 years after release for me to get really hooked. Even more time since I played BOTW. With this new lens, it seems clear to me that Tears is almost a direct upgrade of a game. I'm obsessed.",
     artist: "",
-    pfm: "Switch"
+    pfm: "(Switch)"
 },
 {
     title :"Fallout 3",
     thumb : 1,
     desc : "The sin Bethesda Softworks committed against the Fallout franchise is unforgivable to many. The writing and style of the original 90s CRPGS has slowly been diluted, game by game, until the franchise has reached its mainstream multimedia state today. That began in 2008 with Fallout 3. I briefly played the game shortly after its release as a child and the game terrified me. I have memories of the ghouls, subways, and the power armor, all striking visuals that would haunt me. I started my replay this year to get a new lens in which to see through the posthumous criticism and my own personal nostalgia. There’s still a part of me that loves this game. I appreciate it for it’s excellent tone and art direction to depicting such a desolate world. It’s great to just walk around and soak in the grim environment and soundtrack. (Played modded with Tale of Two Wastelands and a handful of small misc. mods)",
     artist: "",
-    pfm: "PC"
+    pfm: "(PC)"
 }, {
     title : "Metal Gear Solid V: The Phantom Pain",
     thumb : 1,
     desc : "After playing both MGS2, MGS3, and MGSR, I didn't enjoy MGS5 as much. There are much more interlinked complex game systems in a handful of open zone areas, but I personally prefer the more linear focused experiences of the earlier games. MGS5 is not a bad game by any means, just less hooking for me",
     artist: "",
-    pfm: "PC"
+    pfm: "(PC)"
 },
 {
     title : "Marathon",
     thumb : 1,
     desc : "I wish I could dedicate myself more to multiplayer games, because Marathon is truly stunning. It’s art direction and style is cutting edge and completely uncontested. Not to mention the soundtrack and gameplay, both of which stand as stylistic and innovative pillars of their own.",
     artist: "",
-    pfm: "PC"
+    pfm: "(PC)"
 },
 {
     title : "Halo 4",
     thumb : 1,
     desc : "first palythru. Played this on the Master Chief Collection on Heroic with one of my close friends. I was terribly sick during the experience. decent",
     artist: "",
-    pfm: "PC"
+    pfm: "(PC)"
 },
 {
     title : "Shin Megami Tensei Devil Summoner Soul Hackers",
     thumb : 1,
     desc : "I’ve adored my journey through the Shin Megami Tensei series. I was not accustomed to the old-school style of dungeon crawler from the late many of the early megaten titles were. So as my first dungeon crawler, I became addicted to Soul Hackers. The plot and characters don’t have much to write home about, but the gameplay truly scratches something in my brain I haven’t satisfied in a long time. There was an incredibly strike of clarity when I finally understood how the magnetite mechanic works alongside your demon summons. Having to critically think about your team composition to include magnetite, each personalities, skills, and position, all these aspects seem overwhelming at first. But once it all clicked, I couldn’t stop.",
     artist: "",
-    pfm: "3DS"
+    pfm: "(3DS)"
 },
 {
     title : "Miscellaneous",
@@ -97,13 +97,13 @@ let bookList = [
         title: "Persona",
         desc: "This was my first novel of the year, and at the time, the first I had read in 2 years. I enjoyed MOST of this novel. I wasn’t expecting how precise the terminally online twitter/4ch trans woman characterization the main characters would be (the two main trans women had sex with each other right after meeting?). All of the intense horror passages were very hit or miss for me. There were great feelings of dread of sexual assault and Geiger-esque body horror (the last couple of chapters finished the novel <i>wonder</i>fully). But some scenes in the beginning or middle either read as incomprehensible (not in the frightening way) or felt melodramatic and over-dramatized. But despite those criticisms, I did enjoy learning about the dual protagonists and their connection to each other and the truth behind themselves. This novel also acted as the springboard into reading becoming a habit again, and for that at the very least, I will appreciate it for. Recommended if you are one of the aforementioned online trans individuals.",
         artist: "Aoife Josie Clements",
-        pfm: "Bought"
+        pfm: "(Bought)"
     },
     {
         title: "Pale Fire",
         desc: "What led me to this book was my rabbit hole into hypertext / meta-fiction I fell into after reading and falling in love with <i>House of Leaves</i>. Fiction that integrates its medium or authorship in a creative meta-contextual way has been the particular niche of storytelling ever since I was a teenager. This novel has incredible prose (of which Nabokov has been rightfully recognized for) and a wonderfully deceptive narrative framing. I choose to believe in the homoerotic subtext between the narrator and his waxing poet of a muse that the novel revolves around. STILL NEED TO FINISH LOL.",
         artist: "Vladimir Nabokov",
-        pfm: "Library"
+        pfm: "(Library)"
     },
     // {
     //     title: "Go Tell it on the Mountain",
@@ -115,16 +115,16 @@ let bookList = [
         title: "Yokohama",
         desc: "A manga that feels designed in a lab to specifically cater to me. An android woman who runs a coffee cafe and has an unlabeled intimate relationship to a fellow android woman? It’s almost funny. The manga has a great slice-of-life story and atmosphere in a mundane post-apocalyptic world. It’s intensely cute and immersive.",
         artist: "Hitoshi Ashinano",
-        pfm: "Bought"
+        pfm: "(Bought)"
     }, 
     {
         title: "forget-me-not",
         desc: "Prefacing this with the fact that I read this through an unofficial fan translation online. Under the assumption that this translation is accurate, the writing of this manga felt <i>very</i> tight and requires the reader to engage and make some assumptions about the nuanced dialog these characters make. This writing nestled in the center of a lighthearted mystery crime story in Venice made for a great read. Tsuruta’s artstyle is plain gorgeous to look at (I will appreciate any manga or anime that makes its adult women actually look like adults) and incredible ink work in the architecture of this faux-Italy.",
         artist: "Kenji Tsuruta",
-        pfm: "Digital"
+        pfm: "(Digital)"
     }
 ]
 
 function display(item) { // expected array object
-    $("#mediaList").append("<p>" + "<i>" + item.title + "<br>" + item.artist + " (" + item.pfm + ")</i><br>" + item.desc + " </p><br>")
+    $("#mediaList").append("<p>" + "<i>" + item.title + "<br>" + item.artist + " " + item.pfm + "</i><br>" + item.desc + " </p><br>")
 }
